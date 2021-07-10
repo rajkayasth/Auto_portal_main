3360e1be73b40ab8aca41300fc8d152f1f1b70a2
rajkayasth <rajkayasth19@gmail.com>
2021-07-10 21:15:37 +0530

    api connectivity

--- app/src/main/java/com/example/raj2/Adopter/CourseAdapter.java
+++ app/src/main/java/com/example/raj2/Adopter/CourseAdapter.java
@@ -20,39 +20,40 @@ import com.example.raj2.Activity.ModelActivity;
 import com.example.raj2.Activity.MyWishListActivity;
 import com.example.raj2.Activity.ProfileActivity;
 import com.example.raj2.CourseModel;
+import com.example.raj2.Models.car;
 import com.example.raj2.R;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class CourseAdapter extends RecyclerView.Adapter<CourseAdapter.Viewholder> {
 
     private Context context;
-    private ArrayList<CourseModel> courseModelArrayList;
-    private ItemClickListener itemClickListener;
+    private List<car> carComapnylist;
+    //private ItemClickListener itemClickListener;
 
     // Constructor
-    public CourseAdapter(Context context,ArrayList<CourseModel> courseModelArrayList,ItemClickListener item) {
+    public CourseAdapter(Context context, List<car> carComapnylist) {
         this.context = context;
-        this.courseModelArrayList = courseModelArrayList;
-        this.itemClickListener=item;
+        this.carComapnylist = carComapnylist;
+        //this.itemClickListener=item;
     }
 
     @NonNull
     @Override
     public CourseAdapter.Viewholder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         // to inflate the layout for each item of recycler view.
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.cardlayout, parent, false);
+        LayoutInflater inflater=LayoutInflater.from(parent.getContext());
+        View view=inflater.inflate(R.layout.cardlayout,parent,false);
         return new Viewholder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull CourseAdapter.Viewholder holder, int position) {
         // to set data to textview and imageview of each card layout
-        CourseModel model = courseModelArrayList.get(position);
-        holder.courseNameTV.setText(model.getCourse_name());
-        holder.courseIV.setImageResource(model.getCourse_image());
-
-
+        holder.courseNameTV.setText(carComapnylist.get(position).getCompany());
+       // holder.courseIV.setImageResource(model.getCourse_image());
+        /*
         holder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -64,12 +65,16 @@ public class CourseAdapter extends RecyclerView.Adapter<CourseAdapter.Viewholder
             }
 
         });
-
+*/
 //        Log.i("Company",model.getCourse_name().toString());
        /* holder.itemView.setOnClickListener(view -> {
             itemClickListener.onItemClick(courseModelArrayList.get(position));
         });*/
 
+    }
+
+    public void setItems(List<car> carComapnylist) {
+        this.carComapnylist=carComapnylist;
     }
 
     public interface ItemClickListener {
@@ -80,7 +85,7 @@ public class CourseAdapter extends RecyclerView.Adapter<CourseAdapter.Viewholder
     public int getItemCount() {
         // this method is used for showing number
         // of card items in recycler view.
-        return courseModelArrayList.size();
+        return carComapnylist.size();
     }
 
     // View holder class for initializing of
@@ -91,7 +96,7 @@ public class CourseAdapter extends RecyclerView.Adapter<CourseAdapter.Viewholder
 
         public Viewholder(@NonNull View itemView) {
             super(itemView);
-            courseIV = itemView.findViewById(R.id.idIVCourseImage);
+           // courseIV = itemView.findViewById(R.id.idIVCourseImage);
             courseNameTV = itemView.findViewById(R.id.idTVCourseName);
         }
     }
--- app/src/main/java/com/example/raj2/Fregments/HomeFregment.java
+++ app/src/main/java/com/example/raj2/Fregments/HomeFregment.java
@@ -1,5 +1,6 @@
 package com.example.raj2.Fregments;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 
@@ -12,25 +13,36 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.example.raj2.Adopter.CourseAdapter;
+import com.example.raj2.Api.RetrofitClient;
 import com.example.raj2.CompanyDetails;
 import com.example.raj2.CourseModel;
+import com.example.raj2.Models.car;
 import com.example.raj2.R;
+import com.example.raj2.modelResponse.FetchCar_CompanyResponse;
 
 import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
 
 
 public class HomeFregment extends Fragment {
 
-    private RecyclerView courseRV;
+    private RecyclerView recyclerView;
     /*private RecyclerView.Adapter<Course>*/
 
 
-
+    private Context context;
     // Arraylist for storing data
-    private ArrayList<CourseModel> courseModelArrayList;
-
+    private List<car> carList;
+    private CourseAdapter courseAdapter;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -47,13 +59,16 @@ public class HomeFregment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        courseRV = view.findViewById(R.id.idRVCourse);
+        recyclerView = view.findViewById(R.id.idRVCarComapny);
 
+        recyclerView.setHasFixedSize(true);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
 
 
 
         // here we have created new array list and added data to it.
-        courseModelArrayList = new ArrayList<CourseModel>();
+       /* courseModelArrayList = new ArrayList<CourseModel>();
         courseModelArrayList.add(new CourseModel("Maruti Suzuki", R.drawable.marutisuzuki));
         courseModelArrayList.add(new CourseModel("Hyundai",  R.drawable.hyundai));
         courseModelArrayList.add(new CourseModel("Mahindra",  R.drawable.mahindra));
@@ -73,28 +88,36 @@ public class HomeFregment extends Fragment {
         courseModelArrayList.add(new CourseModel("Chevrolet",  R.drawable.chevrolet));
         courseModelArrayList.add(new CourseModel("Isuzu",  R.drawable.isuzu));
         courseModelArrayList.add(new CourseModel("Jeep",  R.drawable.jeep));
+*/
+
+        /*LinearLayoutManager linearLayoutManager= new LinearLayoutManager(context);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(linearLayoutManager);*/
 
 
 
 
 
-        // we are initializing our adapter class and passing our arraylist to it.
-        CourseAdapter courseAdapter = new CourseAdapter(requireContext(), courseModelArrayList, new CourseAdapter.ItemClickListener() {
+        //Retrofit
+        Call<FetchCar_CompanyResponse> call= RetrofitClient.getInstance().getApi().fetchAllcarComapny();
+        call.enqueue(new Callback<FetchCar_CompanyResponse>() {
             @Override
-            public void onItemClick(CourseModel model) {
+            public void onResponse(Call<FetchCar_CompanyResponse> call, Response<FetchCar_CompanyResponse> response) {
+                if (response.isSuccessful()){
+                    carList= response.body().getCarComapnylist();
+                   // recyclerView.setAdapter(courseAdapter);
 
-                Intent intent=new Intent(view.getContext(),CompanyDetails.class);
-                intent.putExtra("company",model.getCourse_name());
-                startActivity(intent);
+                    recyclerView.setAdapter(new CourseAdapter(getActivity(),carList));
+                }
+                else{
+                    Toast.makeText(getActivity(), response.body().getError(), Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<FetchCar_CompanyResponse> call, Throwable t) {
+                Toast.makeText(getActivity(), t.getMessage(), Toast.LENGTH_SHORT).show();
             }
         });
-
-        // below line is for setting a layout manager for our recycler view.
-        // here we are creating vertical list so we will provide orientation as vertical
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(requireActivity(), LinearLayoutManager.VERTICAL, false);
-
-        // in below two lines we are setting layoutmanager and adapter to our recycler view.
-        courseRV.setLayoutManager(linearLayoutManager);
-        courseRV.setAdapter(courseAdapter);
     }
 }
